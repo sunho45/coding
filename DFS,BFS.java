0ba7136453b87274f09c428ab5d0fa972c1fd7ba
sunho45 <100903674+sunho45@users.noreply.github.com>
2022-05-17 00:04:34 +0900

    Update DFS,BFS.java

--- DFS,BFS.java
+++ DFS,BFS.java
@@ -1,27 +1,20 @@
-import java.util.Arrays;
-
-import java.util.StringTokenizer;
-
-
-	
-
-	
-import java.io.*;
 import java.util.*;
-import java.util.Queue;
+
 public class WordSort {
 
 	
 	
 	
 	static int N,E;
-	static int [][] graph=new int[10000][10000];
+	static int [][] graph;
 
-static boolean[] visited=new boolean[10000];
-static boolean[] visitedd=new boolean[10000];
+static boolean[] visited;
+static boolean[] visitedd;
+static int sum=0;
 	public static void dfs(int node) {
 		visited[node]=true;
 		System.out.print(node+" ");
+		
 		for(int i=1;i<=N;i++) {
 			
 			if(!visited[i]&&graph[node][i]==1) {
@@ -39,8 +32,8 @@ static boolean[] visitedd=new boolean[10000];
 		while(!qu.isEmpty()) {
 			
 			int current=qu.remove();
-			
 			System.out.print(current+" ");
+			
 			for(int i=1;i<=N;i++) {
 			if(!visitedd[i]&&graph[current][i]==1) {
 				visitedd[i]=true;
@@ -63,17 +56,21 @@ static boolean[] visitedd=new boolean[10000];
 	
 	
 	
-	public static void main(String[] args)throws IOException {
+	
+	public static void main(String[] args) {
 	
 		
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		
 		
 		
 		Scanner sc=new Scanner(System.in);
 		N=sc.nextInt(); //정점의 개수
 		
 		E=sc.nextInt();// 간선의 개수
-		int init=sc.nextInt();//태초마을
+		int init=sc.nextInt();
+		visited= new boolean[N+1];
+		visitedd= new boolean[N+1];
+		graph= new int[N+1][N+1];
 		for(int i=0;i<E;i++) {
 			int u=sc.nextInt();//정점의 번호 입력 받음
 			int v=sc.nextInt();
