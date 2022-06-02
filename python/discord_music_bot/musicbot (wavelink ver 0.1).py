3df5ac5aa5802abf08d86ed67564dd5fec419cc7
akso1234 <77567577+akso1234@users.noreply.github.com>
2022-06-02 11:14:15 +0900

    Add files via upload

---
+++ python/discord_music_bot/musicbot (wavelink ver 0.1).py
@@ -0,0 +1,101 @@
+import discord
+from discord.ext import commands, tasks
+import os
+from dotenv import load_dotenv
+import youtube_dl
+import asyncio
+
+load_dotenv()
+# Get the API token from the .env file.
+
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='!',intents=intents)
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+ffmpeg_options = {
+    'options': '-vn'
+}
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+async def from_url(url, *, loop=None, stream=False):
+    loop = loop or asyncio.get_event_loop()
+    data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+    if 'entries' in data:
+        # take first item from a playlist
+        data = data['entries'][0]
+    filename = data['title'] if stream else ytdl.prepare_filename(data)
+    return filename
+
+@bot.event
+async def on_ready():
+    print('다음으로 로그인합니다: ')
+    print(bot.user.name)
+    print('connection was succesful')
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("노래 연구"))
+
+@bot.command(name='join')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+  
+@bot.command(name='leave')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+@bot.command(name='play_url')
+async def play(ctx,url):
+    try :
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        async with ctx.typing():
+            filename = await from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+    except:
+        await ctx.send("봇이 음성채널에 정상적으로 들어오지 못했습니다.")
+
+@bot.command(name='pause')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.pause()
+    else:
+        await ctx.send("뮤직봇이 아무 노래도 재생하지 않아 일시정지 기능을 사용할 수 없어요!")
+    
+@bot.command(name='resume')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+    else:
+        await ctx.send("노래가 재생되지 않고 있습니다. 노래를 재생한 후 사용해주세요!")
+
+@bot.command(name='stop', help='Stops the song')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.stop()
+    else:
+        await ctx.send("뮤직봇이 아무 노래도 재생하지 않아 끄기 기능을 사용할 수 없어요!")
+
+bot.run('Nzg4NTg1ODU3NDQ3MzYyNTkx.GKeQYP.3IpaQtfIGFScpjyGFirOtS_ZNyj-d2mJmX8fPY')
